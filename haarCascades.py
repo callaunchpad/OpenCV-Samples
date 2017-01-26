81c1e3c065487ceb1b0689102a82debac8020757
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-01-25 18:35:35 -0800

    Added fullscreen

--- haarCascades.py
+++ haarCascades.py
@@ -1,6 +1,8 @@
 import cv2
 
 camera = cv2.VideoCapture(0)
+
+# Fullscreen
 cv2.namedWindow('Camera', cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty('Camera', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
@@ -22,12 +24,12 @@ while True:
 
     # Sharpen actual frame (OPTIONAL)
     image = gray_frame.copy()
-    gray_frame = cv2.GaussianBlur(image, (0, 0), 3)
+    gray_frame = cv2.GaussianBlur(image, (5, 5), 5)
     gray_frame = cv2.addWeighted(image, 1.5, gray_frame, -0.75, 0)
 
     faces = []
     for classifier in classifiers:
-        for rectangle in classifier.detectMultiScale(gray_frame, scaleFactor = 1.2, minNeighbors = 3):
+        for rectangle in classifier.detectMultiScale(gray_frame, scaleFactor = 1.1, minNeighbors = 1):
             faces.append(rectangle)
 
     for x, y, w, h in faces:
@@ -37,7 +39,7 @@ while True:
 
     cv2.imshow('Camera', frame)
 
-    key = cv2.waitKey(30) & 0xff
+    key = cv2.waitKey(60) & 0xff
 
     # Exit on escape
     if key == 27:
